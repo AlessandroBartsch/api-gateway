66a176c2973852674425c763e909154dacc9a7b4
alessandro.bartsch@jv01.local <alessandro.bartsch@totvs.com.br>
2024-08-12 18:45:44 -0300

    Ajustes nas rotas para aplicacao de load balancer

--- src/main/java/com/ale/api_gateway/configuration/ApiGatewayConfiguration.java
+++ src/main/java/com/ale/api_gateway/configuration/ApiGatewayConfiguration.java
@@ -1,5 +1,6 @@
 package com.ale.api_gateway.configuration;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.Buildable;
@@ -7,6 +8,7 @@ import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.netty.http.client.HttpClient;
 
 import java.util.function.Function;
 
@@ -15,14 +17,16 @@ public class ApiGatewayConfiguration {
 
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
-        Function<PredicateSpec, Buildable<Route>> function =
-                p -> p.path("/get")
+        return builder.routes()
+                .route(p -> p.path("/get")
                         .filters(f -> f
                                 .addRequestHeader("Hello", "World")
                                 .addRequestParameter("Hello", "World"))
-                        .uri("http://httpbin.org:80");
-        return builder.routes()
-                .route(function)
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/cambioservice/**")
+                        .uri("lb://cambioservice"))
+                .route(p -> p.path("/bookservice/**")
+                        .uri("lb://bookservice"))
                 .build();
     }
 
